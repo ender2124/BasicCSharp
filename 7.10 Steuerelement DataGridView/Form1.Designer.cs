091ae0b7f5f39ece4ef67b9472f27b6eddd7b4d0
Felipe <felipe.lopez@fanuc.eu>
2021-11-01 18:21:09 +0100

    7.11 Control DataGridView

---
+++ 7.10 Steuerelement DataGridView/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace _7._10_Steuerelement_DataGridView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DgvPersonen = new System.Windows.Forms.DataGridView();
+            this.CmdInfoSpalte = new System.Windows.Forms.Button();
+            this.LblAnzeige = new System.Windows.Forms.Label();
+            this.CmdMittelwert = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvPersonen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DgvPersonen
+            // 
+            this.DgvPersonen.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvPersonen.Location = new System.Drawing.Point(35, 36);
+            this.DgvPersonen.Name = "DgvPersonen";
+            this.DgvPersonen.Size = new System.Drawing.Size(843, 277);
+            this.DgvPersonen.TabIndex = 0;
+            this.DgvPersonen.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvPersonen_CellContentClick);
+            // 
+            // CmdInfoSpalte
+            // 
+            this.CmdInfoSpalte.Location = new System.Drawing.Point(35, 322);
+            this.CmdInfoSpalte.Margin = new System.Windows.Forms.Padding(6);
+            this.CmdInfoSpalte.Name = "CmdInfoSpalte";
+            this.CmdInfoSpalte.Size = new System.Drawing.Size(138, 42);
+            this.CmdInfoSpalte.TabIndex = 22;
+            this.CmdInfoSpalte.Text = "Info Spalte";
+            this.CmdInfoSpalte.UseVisualStyleBackColor = true;
+            this.CmdInfoSpalte.Click += new System.EventHandler(this.CmdInfoSpalte_Click);
+            // 
+            // LblAnzeige
+            // 
+            this.LblAnzeige.AutoSize = true;
+            this.LblAnzeige.Location = new System.Drawing.Point(257, 331);
+            this.LblAnzeige.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.LblAnzeige.Name = "LblAnzeige";
+            this.LblAnzeige.Size = new System.Drawing.Size(30, 13);
+            this.LblAnzeige.TabIndex = 21;
+            this.LblAnzeige.Text = "(leer)";
+            // 
+            // CmdMittelwert
+            // 
+            this.CmdMittelwert.Location = new System.Drawing.Point(35, 375);
+            this.CmdMittelwert.Margin = new System.Windows.Forms.Padding(6);
+            this.CmdMittelwert.Name = "CmdMittelwert";
+            this.CmdMittelwert.Size = new System.Drawing.Size(138, 42);
+            this.CmdMittelwert.TabIndex = 20;
+            this.CmdMittelwert.Text = "Mittelwert";
+            this.CmdMittelwert.UseVisualStyleBackColor = true;
+            this.CmdMittelwert.Click += new System.EventHandler(this.CmdMittelwert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1010, 455);
+            this.Controls.Add(this.CmdInfoSpalte);
+            this.Controls.Add(this.LblAnzeige);
+            this.Controls.Add(this.CmdMittelwert);
+            this.Controls.Add(this.DgvPersonen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DgvPersonen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DgvPersonen;
+        internal System.Windows.Forms.Button CmdInfoSpalte;
+        internal System.Windows.Forms.Label LblAnzeige;
+        internal System.Windows.Forms.Button CmdMittelwert;
+    }
+}
+
---
+++ 7.10 Steuerelement DataGridView/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _7._10_Steuerelement_DataGridView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            /* Spalten hinzufügen */
+            DgvPersonen.Columns.Add("SpName", "Name");
+            DgvPersonen.Columns.Add("SpVorname", "Vorname");
+            DgvPersonen.Columns.Add("SpPersonalnummer", "Personalnummer");
+            DgvPersonen.Columns.Add("SpGehalt", "Gehalt");
+            DgvPersonen.Columns.Add("SpGeburtstag", "Geburtstag");
+
+            /* Breite einstellen */
+            foreach (DataGridViewTextBoxColumn c in DgvPersonen.Columns)
+                c.Width = 100;
+
+            /* Zeilen hinzufügen */
+            DgvPersonen.Rows.Add(
+            "Maier", "Hans", 6714, 3500.0, "15.03.1962");
+            DgvPersonen.Rows.Add(
+            "Schmitz", "Peter", 81343, 3750.0, "12.04.1958");
+            DgvPersonen.Rows.Add(
+            "Mertens", "Julia", 2297, 3621.5, "30.12.1959");
+            DgvPersonen.Rows.Add(
+           "Felipe", "Lopez", 92000346 , 4539, DateTime.Now.ToShortDateString());
+        }
+
+        private void CmdInfoSpalte_Click(object sender, EventArgs e)
+        {
+            /* Name und Headertext */
+            LblAnzeige.Text = "Name: "
+            + DgvPersonen.Columns["SpName"].Name + ", Header: "
+            + DgvPersonen.Columns["SpName"].HeaderText + "\n";
+            for (int i = 1; i < DgvPersonen.Columns.Count; i++)
+                LblAnzeige.Text += "Name: "
+                + DgvPersonen.Columns[i].Name + ", Header: "
+                + DgvPersonen.Columns[i].HeaderText + "\n";
+        }
+
+        private void CmdMittelwert_Click(object sender, EventArgs e)
+        {
+            double summe = 0, mw;
+            /* Zellen auswerten */
+            LblAnzeige.Text = "";
+            foreach (DataGridViewRow r in DgvPersonen.Rows)
+                summe += Convert.ToDouble(r.Cells[3].Value); //Pillo el valor de la columna 3
+            mw = summe / (DgvPersonen.Rows.Count - 1);
+            LblAnzeige.Text = "Gehalt, Mittelwert: " + mw;
+        }
+
+        private void DgvPersonen_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            /* Klick auswerten */
+            LblAnzeige.Text = "Row: " + e.RowIndex + "\n" +
+            "Colum: " + e.ColumnIndex + "\n";
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+                LblAnzeige.Text += "Inhalt: " +
+                DgvPersonen.Rows[e.RowIndex].Cells[e.ColumnIndex].Value;
+        }
+    }
+}
